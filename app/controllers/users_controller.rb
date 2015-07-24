5052a1d87414fdf588fa99e0cccba7caf5da7f13
behindwisterias <send2goto@gmail.com>
2015-07-24 10:51:22 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
-class UsersController < ApplicationController
-  def show # 追加
+class UsersController < ApplicationController 
+  before_action :set_user, only: [:edit, :update]
+  
+
+  def edit #課題対応で追加 
+    @user = User.find(params[:id])
+  end
+  
+  def update #課題対応で追加
+    if @user.update(user_profile)
+      flash[:success] = "Updated profile"
+      redirect_to @user
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  def show
     @user = User.find(params[:id])
   end
   
@@ -8,20 +25,29 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:info] = "logged in as #{@user.name}"
       redirect_to @user
     else
+      flash[:danger] = 'invalid email/password combination'
       render 'new'
     end
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  
+  def user_profile
+    params.require(:user).permit(:name, :email, :profile)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
   
